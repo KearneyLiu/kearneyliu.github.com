915757972b22db9bc1eabe1a115a04d17d0209ed
Kearney Liu <wmtws9dsj@163.com>
2013-08-18 11:09:11 +0800

    disqus

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,7 +6,7 @@ AUTHOR = u'Kearney Liu'
 SITENAME = u"Kearney Liu's Home"
 SITEURL = 'htpp://www.kearneyliu.com'
 # GITHUB_URL - 'https://github.com/kearneyliu'
-DISQUS_SITENAME = u"wmtws9dsj"
+DISQUS_SITENAME = u"kearneyliu"
 
 RELATIVE_URLS = True
 DEFAULT_PAGINATION = 5
